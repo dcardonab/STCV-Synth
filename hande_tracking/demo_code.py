a745337a7cddf7467f724d53cb884e0ce6d5adba
robert fischer <robert@oakleyrain.com>
2021-11-18 20:47:25 -0600

    more updates

--- hande_tracking/demo_code.py
+++ hande_tracking/demo_code.py
@@ -2,12 +2,12 @@ from handtracking import HandDetector as hd
 import cv2
 import time
 
-def demo_handtracker(wCam=640, hCam=480):
 
-    pTime = 0
+def demo_hand_tracker(w_cam=640, h_cam=480):
+    p_time = 0
     cap = cv2.VideoCapture(0)
-    cap.set(3, wCam)
-    cap.set(4, hCam)
+    cap.set(3, w_cam)
+    cap.set(4, h_cam)
 
     detector = hd(min_detection_confidence=0.75)
 
@@ -17,17 +17,18 @@ def demo_handtracker(wCam=640, hCam=480):
 
         if detector.handCount() > 0:
             for handNumber in range(0, detector.handCount()):
-                lmList = detector.findPosition(img, handNumber=handNumber, draw=False)
-                
-                # Here you can determine the hand and it finger positions, either opened or closed
+                lmList = detector.find_position(img, handNumber=handNumber, draw=False)
+
                 if len(lmList) != 0:
-                    print(detector.isLeftorRightHand(handNumber))
-                    fingersOpen = detector.fingerIsOpen(lmList)
+                    print(detector.is_left_or_right_hand(handNumber))
+                    fingersOpen = detector.finger_is_open(lmList)
                     print(fingersOpen)
 
+                print(detector.find_z_depth(img, handNumber=handNumber))
+
         cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
+        fps = 1 / (cTime - p_time)
+        p_time = cTime
 
         cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 2)
         cv2.imshow("Image", img)
@@ -35,4 +36,5 @@ def demo_handtracker(wCam=640, hCam=480):
 
 
 if __name__ == "__main__":
-    demo_handtracker()
+    demo_hand_tracker()
+
